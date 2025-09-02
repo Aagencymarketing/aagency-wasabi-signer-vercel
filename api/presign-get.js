7743bb2c13da2aefdba21ff1a296ebb7d6492044
Aagencymarketing <aagencysocialmedia@gmail.com>
2025-09-02 14:23:04 +0200

    presign-get.js

--- api/presign-get.js
+++ api/presign-get.js
@@ -2,8 +2,13 @@ import { GetObjectCommand } from "@aws-sdk/client-s3";
 import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
 import { s3, WASABI_BUCKET, SIGNER_TOKEN } from "../s3.mjs";
 
+function setCORS(res) {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, x-agency-token");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, DELETE, OPTIONS");
+}
 function ensureAuth(req, res) {
-  const token = req.headers["x-agency-token"] || req.headers["x-agency-token".toLowerCase()];
+  const token = req.headers["x-agency-token"];
   if (!token || token !== SIGNER_TOKEN) {
     res.statusCode = 401;
     res.setHeader("content-type", "application/json");
@@ -13,20 +18,15 @@ function ensureAuth(req, res) {
   return true;
 }
 
-
 export default async function handler(req, res) {
+  setCORS(res);
+  if (req.method === "OPTIONS") { res.statusCode = 204; return res.end(); }
   if (!ensureAuth(req, res)) return;
   try {
     const urlObj = new URL(req.url, "http://localhost");
     const key = urlObj.searchParams.get("key");
-    if (!key) {
-      res.statusCode = 400;
-      res.end(JSON.stringify({ error: "key required" }));
-      return;
-    }
-    const url = await getSignedUrl(s3, new GetObjectCommand({
-      Bucket: WASABI_BUCKET, Key: key
-    }), { expiresIn: 900 });
+    if (!key) { res.statusCode = 400; return res.end(JSON.stringify({ error: "key required" })); }
+    const url = await getSignedUrl(s3, new GetObjectCommand({ Bucket: WASABI_BUCKET, Key: key }), { expiresIn: 900 });
     res.setHeader("content-type", "application/json");
     res.end(JSON.stringify({ url }));
   } catch (e) {
