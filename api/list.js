5226c59c1c5fca3b1351419d1f3da29b6a62462c
Aagencymarketing <aagencysocialmedia@gmail.com>
2025-09-02 14:14:34 +0200

    list.js

--- api/list.js
+++ api/list.js
@@ -1,8 +1,13 @@
 import { ListObjectsV2Command } from "@aws-sdk/client-s3";
 import { s3, WASABI_BUCKET, SIGNER_TOKEN } from "../s3.mjs";
 
+function setCORS(res) {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, x-agency-token");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, DELETE, OPTIONS");
+}
 function ensureAuth(req, res) {
-  const token = req.headers["x-agency-token"] || req.headers["x-agency-token".toLowerCase()];
+  const token = req.headers["x-agency-token"];
   if (!token || token !== SIGNER_TOKEN) {
     res.statusCode = 401;
     res.setHeader("content-type", "application/json");
@@ -12,8 +17,9 @@ function ensureAuth(req, res) {
   return true;
 }
 
-
 export default async function handler(req, res) {
+  setCORS(res);
+  if (req.method === "OPTIONS") { res.statusCode = 204; return res.end(); }
   if (!ensureAuth(req, res)) return;
   try {
     const url = new URL(req.url, "http://localhost");
